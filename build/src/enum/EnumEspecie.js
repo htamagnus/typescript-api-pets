07bcdbad9f506bd41a59e131371ddd8d903b1f00
Ágatha <agathamagnusg@hotmail.com>
2023-12-09 13:23:52 -0300

    remove build

--- build/src/enum/EnumEspecie.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EnumSpecies;
-(function (EnumSpecies) {
-    EnumSpecies["CACHORRO"] = "cachorro";
-    EnumSpecies["GATO"] = "gato";
-    EnumSpecies["TARTARUGA"] = "tartaruga";
-})(EnumSpecies || (EnumSpecies = {}));
-exports.default = EnumSpecies;
--- build/src/enum/EnumPorte.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EnumSize;
-(function (EnumSize) {
-    EnumSize["pequeno"] = "pequeno";
-    EnumSize["medio"] = "medio";
-    EnumSize["grande"] = "grande";
-})(EnumSize || (EnumSize = {}));
-exports.default = EnumSize;
